afcde44f3a11de524465885c11c89a7359d45852
hzlei <hzlei@foxmail.com>
2020-07-08 22:26:50 +0800

    发布课程-课程分类

--- src/views/edu/course/info.vue
+++ src/views/edu/course/info.vue
@@ -11,6 +11,29 @@
         <el-input v-model="courseInfo.title" placeholder=" 示例：机器学习项目课：从基础到搭建项目视频课程。专业名称注意大小写"/>
       </el-form-item>
       <!-- 所属分类 TODO -->
+      <el-form-item label="课程分类">
+        <!-- 一级分类 -->
+        <el-select
+          v-model="courseInfo.subjectParentId"
+          placeholder="一级分类" @change="subjectLevelOneChanged">
+          <el-option
+            v-for="subject in subjectOneList"
+            :key="subject.id"
+            :label="subject.title"
+            :value="subject.id"/>
+        </el-select>
+        <!-- 二级分类 -->
+        <el-select
+          v-model="courseInfo.subjectId"
+          placeholder="二级分类">
+          <el-option
+            v-for="subject in subjectTwoList"
+            :key="subject.id"
+            :label="subject.title"
+            :value="subject.id"/>
+        </el-select>
+      </el-form-item>
+
       <!-- 课程讲师 TODO -->
       <el-form-item label="课程讲师">
         <el-select
@@ -42,20 +65,33 @@
 </template>
 <script>
 import course from '@/api/course'
+import subject from '@/api/subject'
 export default {
   data() {
     return {
       courseId: '',
       saveBtnDisabled: false, // 保存按钮是否禁用
       teacherList:[], // 所有讲师
+      subjectOneList:[], // 所有课程的一级分类
+      subjectTwoList:[], // 所有课程的二级分类
       courseInfo:{}
     }
   },
   created() {
     // 初始化所有讲师
     this.getListTeacher()
+    // 初始化一级分类
+    this.getOneSubject()
   },
   methods: {
+    // 查询所有课程一级分类
+    getOneSubject() {
+      subject.getSubjectList()
+      .then(response => {
+        this.subjectOneList = response.data.data
+      })
+    },
+
     // 查询所有讲师
     getListTeacher() {
       course.getAllTeacher()
@@ -79,7 +115,17 @@ export default {
          this.$router.push({ path: `/course/chapter/${this.courseId}`})
       })
      
-    }
+    },
+
+    // 一级分类改变时, 改变二级分类的值
+    subjectLevelOneChanged(value) {
+      // value 就是一级分类的 id 值
+      for (let i = 0; i < this.subjectOneList.length; i++) {
+        if (this.subjectOneList[i].id === value) {
+          this.subjectTwoList = this.subjectOneList[i].children
+        }
+      }
+    },
   }
 }
 </script>
